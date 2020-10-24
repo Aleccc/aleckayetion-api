a9666f6b8da5a0ce86b228766f4e0c3cdd2fa3f1
alec <alec.kaye@gmail.com>
2020-10-24 16:12:46 -0400

    add gunicorn

--- api/settings.py
+++ api/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', ]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'aleckayetion-api.herokuapp.com']
 
 
 # Application definition
