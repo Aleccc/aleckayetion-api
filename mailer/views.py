0db0bd35fce672867fc32be607ed0e194711d0bf
Aleccc <alec.kaye@gmail.com>
2020-10-24 17:21:41 -0400

    Update views.py

--- mailer/views.py
+++ mailer/views.py
@@ -1,19 +1,23 @@
 from django.core import mail
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 
 @api_view(['POST', ])
 def send(request):
-    if request.method == 'POST':
-        subject = request.POST.get('subject', '')
-        body = request.POST.get('body', '')
-        if subject and body:
-            try:
-                mail.send_mail(subject, body, 'alec@aleckayetion.com', ['alec@aleckayetion.com'])
-            except mail.BadHeaderError:
-                return False
-        return True  # Response({"message": "Got some data!", "data": request.data})
-    return False
+    subject = request.POST.get('subject', '')
+    body = request.POST.get('body', '')
+    if subject and body:
+        try:
+            mail.send_mail(subject, body, 'alec@aleckayetion.com', ['alec@aleckayetion.com'])
+            content = {'message': 'sent'}
+            return Response(content, status=status.HTTP_202_ACCEPTED)
+        except mail.BadHeaderError:
+            content = {'message': 'BadHeaderError'}
+            return Response(content, status=status.HTTP_400_BAD_REQUEST)
+    content = {'message': 'no subject/body'}
+    return Response(content, status=status.HTTP_400_BAD_REQUEST)
 
 
 # with mail.get_connection() as connection:
