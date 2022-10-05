c85a3c47780694554412f69e0cab628b3c4aa021
alec <alec.kaye@gmail.com>
2022-10-04 20:52:25 -0400

    add 'to' parameter for send_email

--- mailer/views.py
+++ mailer/views.py
@@ -1,29 +1,33 @@
 from django.core import mail
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
 from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
 @api_view(['POST', ])
 def send(request):
     subject = request.data.get('subject', '')
     body = request.data.get('body', '')
-    if subject and body:
-        try:
-            mail.send_mail(subject, body, 'alec@aleckayetion.com', ['alec@aleckayetion.com'])
-            content = {'message': 'sent'}
-            return Response(content, status=status.HTTP_202_ACCEPTED)
-        except mail.BadHeaderError:
-            content = {'message': 'BadHeaderError'}
-            return Response(content, status=status.HTTP_400_BAD_REQUEST)
-    content = {'message': 'no subject/body',
-              'data': request.data}
-    return Response(content, status=status.HTTP_400_BAD_REQUEST)
+    to = request.data.get('to', 'alec@aleckayetion.com')
+    to = [x.strip() for x in to.split(',')]
 
+    try:
+        for x in to:
+            validate_email(x)
+    except ValidationError:
+        content = {'message': 'invalid email included', 'data': str(to)}
+        return Response(content, status=status.HTTP_400_BAD_REQUEST)
 
-# with mail.get_connection() as connection:
-#     mail.EmailMessage(
-#         # subject1, body1, from1, [to1],
-#         'test', 'body', 'no-reply@aleckayetion.com', ['alec.kaye@gmail.com',],
-#         connection=connection,
-#     ).send()
+    if not subject and not body:
+        content = {'message': 'no subject/body', 'data': request.data}
+        return Response(content, status=status.HTTP_400_BAD_REQUEST)
+
+    try:
+        mail.send_mail(subject, body, 'alec@aleckayetion.com', to)
+        content = {'message': 'sent'}
+        return Response(content, status=status.HTTP_202_ACCEPTED)
+    except mail.BadHeaderError:
+        content = {'message': 'BadHeaderError'}
+        return Response(content, status=status.HTTP_400_BAD_REQUEST)
